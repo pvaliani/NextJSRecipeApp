d0d052288ca3279ae0b6548aefe5bd0e364ca151
Pedram Homayoun-Valiani <pedram.homayoun-valiani@bbc.co.uk>
2021-06-25 13:05:02 +0100

    adds basic styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import Link from "next/link";
+
+// This file encompasses the entire nextJS app
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav className="header">
+        <div>
+          <Link href="/">
+            <a> My Kitchen 👨🏼‍🍳 </a>
+          </Link>
+        </div>
+      </nav>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
 
 export default MyApp
